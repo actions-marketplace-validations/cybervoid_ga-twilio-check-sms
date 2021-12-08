d77aa68ff0d4ef55c9d683ddc634fc1b3156ef00
Rafael Gil <rafaelgil83@gmail.com>
2021-12-08 18:31:25 -0500

    added couple of small code improvements

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
 
-const twilio = require('twilio');
-const client = new twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-client.messages()
+// client.messages.fetch()
 
 client.messages
     .create({
