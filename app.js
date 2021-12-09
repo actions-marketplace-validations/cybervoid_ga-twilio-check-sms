fd1f2094ff00ed1c1b98620832a7e19305adbc69
Rafael Gil <rafaelgil83@gmail.com>
2021-12-08 20:10:31 -0500

    added github actions

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
-const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+// const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+// const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+// const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
 
-const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+// const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-// client.messages.fetch()
 
-client.messages
-    .create({
-        body: 'Hello from Node',
-        to: '', // Text this number
-        from: TWILIO_PHONE_NUMBER,
-    })
-    .then((message) => console.log(message.sid));
+try {
+
+
+    // client.messages.list({
+    //     to: TWILIO_PHONE_NUMBER,
+    //     limit: 20
+    // })
+    //     .then(messages => messages.forEach(m => {
+    //
+    //         console.log(m.body)
+    //     }))
+    //     .catch(err => console.log(`Error in the call detected`))
+
+
+    const nameToGreet = core.getInput('phone');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
+
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+
